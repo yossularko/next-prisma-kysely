2c50d83aeb75f2f7ffb15b959e167b19183e86ee
yossularko <yossularko@gmail.com>
2025-01-31 09:57:43 +0700

    add: example query select jsonArray

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/lib/database";
+import { jsonArrayFrom } from "kysely/helpers/postgres";
 import HomePage from "./HomePage";
 
 export default async function Home() {
@@ -6,20 +7,29 @@ export default async function Home() {
     db
       .selectFrom("Post")
       .innerJoin("User", "User.id", "Post.authorId")
-      .select([
-        "Post.id",
-        "title",
-        "Post.content",
-        "published",
-        "createdAt",
-        "updatedAt",
-        "authorId",
-        "User.name as author",
-      ])
+      .selectAll("Post")
+      .select(["User.name as author"])
       .orderBy("createdAt", "desc")
       .execute(),
     db.selectFrom("User").selectAll().execute(),
   ]);
+
+  const postByUser = await db
+    .selectFrom("User")
+    .selectAll("User")
+    .select((eb) => {
+      return [
+        jsonArrayFrom(
+          eb
+            .selectFrom("Post")
+            .selectAll("Post")
+            .whereRef("Post.authorId", "=", "User.id")
+        ).as("posts"),
+      ];
+    })
+    .execute();
+
+  console.log("post by user: ", JSON.stringify(postByUser, null, 2));
 
   return <HomePage posts={posts} users={users} />;
 }
